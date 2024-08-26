2c890b0036976f323039cd436ab159f807f3acb0
Wesley Santos <wesleysotnas64@gmail.com>
2024-08-26 17:30:21 -0300

    feat: Novas direções iniciais da bola.

--- pong-clone-portfolio/Assets/Scripts/Ball.cs
+++ pong-clone-portfolio/Assets/Scripts/Ball.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
@@ -9,11 +10,13 @@ public class Ball : MonoBehaviour
     public float speedPercentage;
     public Vector2 initialPosition;
     public bool idle;
+    public List<Vector2> initialDirections;
     
 
     void Start()
     {
         initialPosition = new Vector2(transform.position.x, transform.position.y);
+        InitInitialDirections();
         Reset();
     }
 
@@ -46,23 +49,11 @@ public class Ball : MonoBehaviour
 
     private void RandomInitialDirection()
     {
-        do
-        {
-            direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        direction = initialDirections[Random.Range(0, initialDirections.Count)];        
 
-            direction = direction.normalized;
-
-        } while (IsInvalidDirection(direction));
+        transform.position = transform.position + new Vector3(0, Random.Range(-3, 3), 0);
 
         idle = false;
-    }
-
-    private bool IsInvalidDirection(Vector2 direction)
-    {
-        return (Mathf.Approximately(direction.x, 1f) && Mathf.Approximately(direction.y, 0f)) ||
-            (Mathf.Approximately(direction.x, -1f) && Mathf.Approximately(direction.y, 0f)) ||
-            (Mathf.Approximately(direction.x, 0f) && Mathf.Approximately(direction.y, 1f)) ||
-            (Mathf.Approximately(direction.x, 0f) && Mathf.Approximately(direction.y, -1f));
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -86,5 +77,47 @@ public class Ball : MonoBehaviour
     private Vector2 ReflectDirection(Vector2 _direction, Vector2 _normal)
     {
         return _direction - 2 * Vector2.Dot(_direction, _normal) * _normal;
+    }
+
+    private void InitInitialDirections()
+    {
+        //Cardinais principais
+        Vector2 north = new Vector2(0, 1).normalized;
+        Vector2 south = new Vector2(0, -1).normalized;
+        Vector2 east = new Vector2(1, 0).normalized;
+        Vector2 west = new Vector2(-1, 0).normalized;
+        
+        //Cardinais intermediários
+        Vector2 northeast = new Vector2(1, 1).normalized;
+        Vector2 northwest = new Vector2(-1, 1).normalized;
+        Vector2 southeast = new Vector2(1, -1).normalized;
+        Vector2 southwest = new Vector2(-1, -1).normalized;
+
+        // Intermediários entre cardinais e intermediários
+        Vector2 north_northeast = (north + northeast).normalized;
+        Vector2 east_northeast = (northeast + east).normalized;
+        Vector2 east_southeast = (east + southeast).normalized;
+        Vector2 south_southeast = (southeast + south).normalized;
+        Vector2 south_southwest = (south + southwest).normalized;
+        Vector2 west_southwest = (southwest + west).normalized;
+        Vector2 west_northwest = (west + northwest).normalized;
+        Vector2 north_northwest = (northwest + north).normalized;
+
+        initialDirections = new List<Vector2>
+        {
+            northeast,
+            northwest,
+            southeast,
+            southwest,
+            north_northeast,
+            east_northeast,
+            east_southeast,
+            south_southeast,
+            south_southwest,
+            west_southwest,
+            west_northwest,
+            north_northwest
+        };
+        
     }
 }
